6a1e8a260de9e25df60fa5617fa74ce22fc42158
v-sakmoh <66132934+v-sakmoh@users.noreply.github.com>
2020-09-23 15:38:55 -0800

    Launcher Sample (#13)

--- DeveloperLabs/MyEmployees/MyEmployees/Entities/Employee.cs
+++ DeveloperLabs/MyEmployees/MyEmployees/Entities/Employee.cs
@@ -6,5 +6,6 @@
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
+        public string Address { get; set; }
     }
 }
