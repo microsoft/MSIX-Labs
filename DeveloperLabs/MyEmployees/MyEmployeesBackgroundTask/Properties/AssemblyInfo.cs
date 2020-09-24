bcfcc8ce7dd8a5aff56a397185acc8488735a5de
Sakariya Mohamed (WTIA Workforce Institute) <v-sakmoh@microsoft.com>
2020-09-24 16:50:10 -0700

    Updated BackgroundTask Project

--- DeveloperLabs/MyEmployees/MyEmployeesBackgroundTask/Properties/AssemblyInfo.cs
+++ DeveloperLabs/MyEmployees/MyEmployeesBackgroundTask/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BackgroundUpdate.Head")]
+[assembly: AssemblyTitle("MyEmployeesBackgroundTask")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BackgroundUpdate.Head")]
+[assembly: AssemblyProduct("MyEmployeesBackgroundTask")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
